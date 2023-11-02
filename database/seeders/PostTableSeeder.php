d0c9087b96ec9c31d4d19d4982c07f8da26b2296
Joshua Richardson <j22richo@gmail.com>
2023-11-02 17:44:22 +0000

    Seeded post table

---
+++ database/seeders/PostTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $post = new Post;
+        $post->text = "Lorem ipsum dolar sit amet consecutar";
+        $post->likes = 10;
+        $post->dislikes = 2;
+        $post->save();
+    }
+}
